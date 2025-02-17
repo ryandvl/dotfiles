aca2de7cecc62c98e99821c52ca3aec69b8d442a
ryandvl <ryandvl@proton.me>
2025-02-17 18:39:36 -0300

    feat(fish): add "fnm" alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,11 @@
 # Remove fish greeting message
 set -g fish_greeting
 
-# Aliases
-alias "g" "git"
-alias "py" "python3"
-alias "prisma" "pnpm prisma"
+# Commands aliases
+alias "g" "git"                 # Git (https://git-scm.com/)
+alias "n" "fnm"                 # NodeJS (https://nodejs.org/) with Fast Node Manager (https://github.com/Schniz/fnm)
+alias "py" "python3"            # Python (https://www.python.org/)
+alias "prisma" "pnpm prisma"    # Prisma (https://www.prisma.io/)
 
 # Initialize starship (https://starship.rs/)
 starship init fish | source
