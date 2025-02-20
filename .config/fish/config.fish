a198a43602c2b86ff7862b9b1ea45a7a652f041a
ryandvl <ryandvl@proton.me>
2025-02-20 16:38:50 -0300

    feat(fish): add "gc" alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,17 @@
 set -g fish_greeting
 
 # Commands aliases
-alias "g" "git"                 # Git (https://git-scm.com/)
-alias "n" "fnm"                 # NodeJS (https://nodejs.org/) with Fast Node Manager (https://github.com/Schniz/fnm)
-alias "py" "python3"            # Python (https://www.python.org/)
+
+## Git (https://git-scm.com/)
+alias "g" "git"
+alias "gc" "git commit -m"
+
+## NodeJS (https://nodejs.org/)
+alias "n" "fnm"                 # Fast Node Manager (https://github.com/Schniz/fnm)
 alias "prisma" "pnpm prisma"    # Prisma (https://www.prisma.io/)
+
+## Python (https://www.python.org/)
+alias "py" "python3"
 
 # Initialize starship (https://starship.rs/)
 starship init fish | source
