9abc6052127d3bcd7f10dce2cc7795494c5ee4d9
ryandvl <ryandvl@proton.me>
2025-03-09 22:46:08 -0300

    feat(nvim): update keymaps & lazy extras

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,20 @@
 local set = vim.keymap.set
+local del = vim.keymap.del
 local options = {
-  noremap = true,
-  silent = true,
+	noremap = true,
+	silent = true,
 }
 local Snacks = require("snacks")
+
+-- LazyVim
+del("n", "<leader>l") -- Remove Lazy (Space + l)
+del("n", "<leader>L") -- Remove Lazy Changelog (Space + L)
+
+set("n", "<leader>ll", "<Cmd>Lazy<CR>", { desc = "Lazy" }) -- (Space + l + l) Lazy
+set("n", "<leader>lc", function()
+	LazyVim.news.changelog()
+end, { desc = "LazyVim Changelog" }) -- (Space + l + c) LazyVim Changelog
+set("n", "<leader>lx", "<Cmd>LazyExtras<CR>", { desc = "LazyExtras" }) -- (Space + l + x) Lazy Extras
 
 -- Increment/Decrement
 set("n", "+", "<C-a>") -- (+) Increment
@@ -16,7 +27,7 @@ set("n", "<C-a>", "gg<S-v>G") -- (Ctrl + a) Select All
 set("n", "<S-tab>", "<Cmd>bp<CR>", options) -- (Shift + Tab) Previous Buffer
 set("n", "<tab>", "<Cmd>bn<CR>", options) -- (Tab) Next Buffer
 set("n", "<C-w>", function()
-  Snacks.bufdelete()
+	Snacks.bufdelete()
 end, options) -- (Ctrl + w) Close Buffer
 
 -- Neotree
@@ -24,4 +35,7 @@ set("n", "<leader>e", "<Cmd>Neotree toggle<CR>", options) -- (Space + e) Toggle 
 set("n", "<leader>E", "<Cmd>Neotree focus<CR>", options) -- (Space + E) Focus NeoTree
 
 -- Quit
-set("n", "<C-Del>", "<Cmd>quitall<CR>", options) -- Quit All
+set("n", "<C-Del>", "<Cmd>quitall<CR>", options) -- (Ctrl + Delete) Quit All
+
+-- Incremental LSP Renaming
+set("n", "<leader>rn", ":IncRename ") -- (Space + r + n) Inc Rename
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,52 +1,77 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+	spec = {
+		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 
-    -- Add Extra LazyVim Plugins
+		-- Add Extra LazyVim Plugins
 
-    -- Languages:
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
+		-- Languages:
+		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.typescript" },
+		{ import = "lazyvim.plugins.extras.lang.tailwind" },
+		{ import = "lazyvim.plugins.extras.lang.prisma" },
 
-    { import = "plugins" },
-  },
-  defaults = {
-    lazy = false,
-    version = false,
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+		-- { import = "lazyvim.plugins.extras.lang.python" },
+
+		{ import = "lazyvim.plugins.extras.lang.go" },
+
+		-- { import = "lazyvim.plugins.extras.lang.java" },
+		-- { import = "lazyvim.plugins.extras.lang.kotlin" },
+
+		{ import = "lazyvim.plugins.extras.lang.docker" },
+
+		-- Formating
+		{ import = "lazyvim.plugins.extras.formatting.prettier" },
+
+		-- Linting
+		{ import = "lazyvim.plugins.extras.linting.eslint" },
+
+		-- Editor
+		{ import = "lazyvim.plugins.extras.editor.inc-rename" },
+
+		-- Utils
+		{ import = "lazyvim.plugins.extras.util.dot" },
+
+		-- Others
+		{ import = "lazyvim.plugins.extras.vscode" },
+
+		{ import = "plugins" },
+	},
+	defaults = {
+		lazy = false,
+		version = false,
+	},
+	install = { colorscheme = { "tokyonight", "habamax" } },
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
