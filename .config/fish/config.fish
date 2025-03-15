fa648e54ab3db57d9730cd5c54ecc3a9f08c39f3
ryandvl <ryandvl@proton.me>
2025-03-14 21:57:09 -0300

    feat(fish): update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,26 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 # Remove fish greeting message
 set -g fish_greeting
 
 # Commands aliases
 
 ## Git (https://git-scm.com/)
-alias "g" "git"
-alias "gc" "git commit -m"
+alias g git
+alias gc "git commit -m"
+alias gac "git add . && git commit -m"
 
 ## NodeJS (https://nodejs.org/)
-alias "n" "fnm"                 # Fast Node Manager (https://github.com/Schniz/fnm)
-alias "prisma" "pnpm prisma"    # Prisma (https://www.prisma.io/)
+alias nvm fnm # Fast Node Manager (https://github.com/Schniz/fnm)
+alias prisma "pnpm prisma" # Prisma (https://www.prisma.io/)
 
 ## Python (https://www.python.org/)
-alias "py" "python3"
+alias py python3
+
+## Neovim (https://neovim.io/)
+alias n nvim
 
 # Initialize starship (https://starship.rs/)
 starship init fish | source
